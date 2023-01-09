09a661fd6dd66d57190fece7afa07db835349c52
solov <soloviov.alexandr2012@yandex.ru>
2023-01-09 17:17:55 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,22 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] rsl = new Item[items.length];
+        int num = 0;
+        for (Item item : items) {
+            if (item != null && key.equals(item.getName())) {
+                rsl[num] = item;
+                num++;
+            }
+        }
+        return Arrays.copyOf(rsl, num);
     }
 
     public Item findById(int id) {
