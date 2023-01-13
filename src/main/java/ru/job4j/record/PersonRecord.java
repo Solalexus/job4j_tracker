7315cfb7ea5ac363c3cc0d911c583b86289e1906
solov <soloviov.alexandr2012@yandex.ru>
2023-01-13 13:23:28 +0300

    11. Record [#504880 #380760]

---
+++ src/main/java/ru/job4j/record/PersonRecord.java
@@ -0,0 +1,8 @@
+package ru.job4j.record;
+
+public record PersonRecord(String name, int age) {
+    @Override
+    public String toString() {
+        return "Person{" + "name='" + name + '\'' + ", age=" + age + '}';
+    }
+}
